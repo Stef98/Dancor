181d32bd56ca2ef7b5f34a99b9a084b8cc068dde
Stef98 <stefferreira14@gmail.com>
2023-08-20 10:08:54 +0200

    Header and footer rrework

--- js/main.js
+++ js/main.js
@@ -78,4 +78,19 @@ $(document).ready(function () {
     });
 });
 
+fetch("./Header.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+fetch("./Footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
+
 
